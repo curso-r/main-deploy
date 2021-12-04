4e1a366327d88c6ecb8fcf3c3530a1d08bc1795d
C. Lente <7017340+clente@users.noreply.github.com>
2021-12-04 12:10:25 -0300

    Novo exemplo de golem com deploy para o Shinyapps

--- arqs/04_exemplo_golem/R/app_config.R
+++ arqs/04_exemplo_golem/R/app_config.R
@@ -12,7 +12,6 @@ app_sys <- function(...){
   system.file(..., package = "exemplogolem")
 }
 
-
 #' Read App Config
 #' 
 #' @param value Value to retrieve from the config file. 
---
+++ arqs/04_exemplo_golem/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(exemplogolem)
+
+test_check("exemplogolem")
