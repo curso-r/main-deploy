be858490402218048fd84a07530bf180207a5075
C. Lente <7017340+clente@users.noreply.github.com>
2022-10-03 14:15:31 -0300

    Update examples

--- arqs/05_exemplo_deploy/R/app_server.R
+++ arqs/05_exemplo_deploy/R/app_server.R
@@ -1,10 +1,20 @@
 #' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
+#'
+#' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-app_server <- function( input, output, session ) {
-  # Your application server logic 
-  
+app_server <- function(input, output, session) {
+  # Your application server logic
+
+  output$distPlot <- renderPlot({
+
+    x    <- datasets::faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    graphics::hist(x, breaks = bins, col = "#75AADB", border = "white",
+                   xlab = "Waiting time to next eruption (in mins)",
+                   main = "Histogram of waiting times")
+
+  })
 }
--- arqs/05_exemplo_deploy/R/app_ui.R
+++ arqs/05_exemplo_deploy/R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
@@ -8,35 +8,57 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # Your application UI logic 
-    fluidPage(
-      h1("exemplodeploy")
+    # Your application UI logic
+    # App title ----
+    titlePanel("Hello Shiny!"),
+
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+
+      # Sidebar panel for inputs ----
+      sidebarPanel(
+
+        # Input: Slider for the number of bins ----
+        sliderInput(inputId = "bins",
+                    label = "Number of bins:",
+                    min = 1,
+                    max = 50,
+                    value = 30)
+
+      ),
+
+      # Main panel for displaying outputs ----
+      mainPanel(
+
+        # Output: Histogram ----
+        plotOutput(outputId = "distPlot")
+
+      )
     )
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
-golem_add_external_resources <- function(){
-  
+golem_add_external_resources <- function() {
   add_resource_path(
-    'www', app_sys('app/www')
+    "www",
+    app_sys("app/www")
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'exemplodeploy'
+      path = app_sys("app/www"),
+      app_title = "exemplogolem"
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
-
